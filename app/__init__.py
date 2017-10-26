a1bf3e7bda959cdfea069d7f275f966563f1fc9b
Soumyajit Dutta <sanudatta11@gmail.com>
2017-10-26 09:58:55 +0530

    Added CSRF

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
-application = app
 app = Flask(__name__)
+application = app
 csrf = CSRFProtect(app)
 app.config.from_object('config')
 db = SQLAlchemy(app)
