3557bcccd02052b10596c025a2d2a2a89ca94e85
Soumyajit Dutta <sanudatta11@gmail.com>
2017-10-26 19:34:41 +0530

    Added 500 Error

--- app/static/js/error404.js
+++ app/static/js/error404.js
@@ -1,6 +1,7 @@
-$(document).ready(function () {
-	$("#image404").toggleClass('flip');
-	$("#error-heading").toggleClass('flip-again');
-	$("#page_not_found_image").toggleClass('flap');
-	$("#csrf_error").toggleClass('flap-again');
-});
+	$(document).ready(function () {
+		$("#image404").toggleClass('flip');
+		$("#error-heading").toggleClass('flip-again');
+		$("#page_not_found_image").toggleClass('flap');
+		$("#csrf_error").toggleClass('flap-again');
+		$("#error_500").toggleClass('flop');
+	});
