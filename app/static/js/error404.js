6493a138b41fa877d89e6367863e8318a500ee67
Soumyajit Dutta <sanudatta11@gmail.com>
2017-10-26 18:44:53 +0530

    Added CSRF Page

--- app/static/js/error404.js
+++ app/static/js/error404.js
@@ -1,5 +1,6 @@
-$(document).ready(function() {
- 		$("#image404").toggleClass('flip');
-		$("#error-heading").toggleClass('flip-again');
-		$("#page_not_found_image").toggleClass('flap');
-	});
+$(document).ready(function () {
+	$("#image404").toggleClass('flip');
+	$("#error-heading").toggleClass('flip-again');
+	$("#page_not_found_image").toggleClass('flap');
+	$("#csrf_error").toggleClass('flap-again');
+});
