1f16543cbdd791354fcf8d436926326cb81c4c98
Soumyajit Dutta <sanudatta11@gmail.com>
2017-11-07 06:17:27 +0530

    Trying Mitten

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,8 @@
 from flask import Flask
+from flask.ext.mitten import Mitten
+
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
-from flask_wtf.csrf import CSRFProtect
 app = Flask(__name__)
 application = app
 login_manager = LoginManager()
@@ -10,7 +11,9 @@ login_manager.init_app(app)
 login_manager.login_view = "login"
 login_manager.session_protection = "strong"
 
-csrf = CSRFProtect(app)
+mitten = Mitten(app)
+mitten.banner = "Short To Banner!"
+
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
