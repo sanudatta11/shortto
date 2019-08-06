0e0983d58924f8166d27ce23de1ba3f0ca4135d9
Soumyajit Dutta <sanudatta11@gmail.com>
2019-08-06 12:56:41 +0530

    Added tooltips

---
+++ app/static/js/error404.js
@@ -0,0 +1,7 @@
+	$(document).ready(function () {
+		$("#image404").toggleClass('flip');
+		$("#error-heading").toggleClass('flip-again');
+		$("#page_not_found_image").toggleClass('flap');
+		$("#csrf_error").toggleClass('flap-again');
+		$("#error_500").toggleClass('flop');
+	});
