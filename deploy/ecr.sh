0d8b6facdb258c1531b17fcf04b86b8fbb7ef396
Soumyajit Dutta <sanudatta11@gmail.com>
2020-09-11 02:35:33 +0530

    Added AWS 2nd Account deployment

--- deploy/ecr.sh
+++ deploy/ecr.sh
@@ -1,13 +1,16 @@
 #!/bin/bash -e
+export AWS_DEFAULT_REGION=${REGION_NEW}
+export AWS_ACCESS_KEY_ID=${AWS_ACCESS_KEY_ID_NEW}
+export AWS_SECRET_ACCESS_KEY=${AWS_SECRET_ACCESS_KEY_NEW}
 
 # the registry should have been created already
 # you could just paste a given url from AWS but I'm
 # parameterising it to make it more obvious how its constructed
-REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com
+REGISTRY_URL=${AWS_ACCOUNT_ID}.dkr.ecr.${REGION_NEW}.amazonaws.com
 # this is most likely namespaced repo name like myorg/veryimportantimage
-SOURCE_IMAGE="${DOCKER_REPO}"
+SOURCE_IMAGE="${DOCKER_REPO_NEW}"
 # using it as there will be 2 versions published
-TARGET_IMAGE="${REGISTRY_URL}/${DOCKER_REPO}"
+TARGET_IMAGE="${REGISTRY_URL}/${DOCKER_REPO_NEW}"
 # lets make sure we always have access to latest image
 TARGET_IMAGE_LATEST="${TARGET_IMAGE}:latest"
 TIMESTAMP=$(date '+%Y%m%d%H%M%S')
@@ -19,7 +22,7 @@ VERSION="${TIMESTAMP}-${TRAVIS_COMMIT}"
 TARGET_IMAGE_VERSIONED="${TARGET_IMAGE}:${VERSION}"
 
 # making sure correct region is set
-aws configure set default.region ${REGION}
+aws configure set default.region ${REGION_NEW}
 
 # Push image to ECR
 ###################
@@ -38,5 +41,5 @@ docker tag ${SOURCE_IMAGE} ${TARGET_IMAGE_VERSIONED}
 docker push ${TARGET_IMAGE_VERSIONED}
 
 #Cleaning Untagged Images
-IMAGES_TO_DELETE=$( aws ecr list-images --region ${REGION} --repository-name ${DOCKER_REPO} --filter "tagStatus=UNTAGGED" --query 'imageIds[*]' --output json )
-aws ecr batch-delete-image --region ${REGION} --repository-name ${DOCKER_REPO} --image-ids "$IMAGES_TO_DELETE" || true
+IMAGES_TO_DELETE=$( aws ecr list-images --region ${REGION_NEW} --repository-name ${DOCKER_REPO_NEW} --filter "tagStatus=UNTAGGED" --query 'imageIds[*]' --output json )
+aws ecr batch-delete-image --region ${REGION_NEW} --repository-name ${DOCKER_REPO_NEW} --image-ids "$IMAGES_TO_DELETE" || true
